87f2bd9db67da91ed99682e5fb956b2ce2983b86
Pranav P.S <pranav.pavithran@trenser.com>
2022-06-22 13:13:10 +0530

    first try

---
+++ app/src/main/java/com/myapplication/expensetracker/SpalshLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.myapplication.expensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpalshLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh_login)
+    }
+}
